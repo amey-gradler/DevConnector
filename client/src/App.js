9f227fbff2c9ce1937e82fcf27da0c82c08e32a6
ameyA <ameyaganore@gmail.com>
2021-12-03 19:34:46 +0530

    react authentication done

---
+++ client/src/App.js
@@ -0,0 +1,47 @@
+import React, { Fragment, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Alert from './components/layout/Alert';
+//redux
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+import './App.css';
+import setAuthToken from './utils/setAuthToken';
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Routes>
+            <Route exact path='/' element={<Landing />} />
+          </Routes>
+
+          <section className='container'>
+            <Alert />
+            <Routes>
+              <Route exact path='/register' element={<Register />} />
+
+              <Route exact path='/login' element={<Login />} />
+            </Routes>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
+export default App;
