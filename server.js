0256597c55cb707dabe2bfd23e74f3c72750118f
ameyA <ameyaganore@gmail.com>
2021-12-08 10:11:36 +0530

    preparing for deployment

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
 //Serve static assets in production
-if (process.eventNames.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   //set static folder
   app.use(express.static('client/build'));
 
